9f78891d77348413000eac5019cd752f684f736f
negativeInteger <139580532+INFERNO1211@users.noreply.github.com>
2025-03-30 00:46:31 +0530

    added defaults in swagger docs

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,27 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Dict, Any, Optional
 
 class QueryRequest(BaseModel):
-    query: str
+    query: str = Field(..., example="Get all transactions where amount is greater than 300")
 
 class QueryResponse(BaseModel):
-    query: str
-    data: Optional[List[Dict[str, Any]]] = []
+    query: str = Field(..., example="Get all transactions where amount is greater than 300")
+    data: Optional[List[Dict[str, Any]]] = Field(default=[], example=[{"id": 1, "amount": 500}])
 
 class ExplainResponse(BaseModel):
-    query: str
-    breakdown: Dict[str, Optional[str]]
+    query: str = Field(..., example="Sort sales by region in ascending order")
+    breakdown: Dict[str, Optional[str]] = Field(..., example={"operation": "sort", "field": "region", "order": "ascending"})
 
 class ValidateResponse(BaseModel):
-    query: str
-    valid: bool
+    query: str = Field(..., example="Get all customers")
+    valid: bool = Field(..., example=False)
 
 class User(BaseModel):
-    email: str
+    email: str = Field(..., example="testuser@example.com")
 
 class CreatedUser(BaseModel):
-    email: str
-    api_key: str
+    email: str = Field(..., example="testuser@example.com")
+    api_key: str = Field(..., example="123e4567-e89b-12d3-a456-426614174000")
 
 class ApiKey(BaseModel):
-    api_key: str
+    api_key: str = Field(..., example="123e4567-e89b-12d3-a456-426614174000")
